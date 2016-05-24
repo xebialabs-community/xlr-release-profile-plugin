e9e81a85453f0612f9e1a54585af00733a0ee2af
wianvos <wvos@xebia.com>
2016-05-24 11:36:27 -0400

    added the ability to crate and destroy counter stores

--- src/main/resources/rel/ReleaseCounter.py
+++ src/main/resources/rel/ReleaseCounter.py
@@ -8,16 +8,72 @@ import com.xebialabs.xlrelease.domain.Configuration as Configuration
 import com.xebialabs.deployit.plugin.api.reflect.Type as Type
 import com.xebialabs.deployit.jcr
 import com.xebialabs.deployit.repository
+from com.xebialabs.deployit.plugin.api.reflect import DescriptorRegistry
+from com.xebialabs.deployit.plugin.api.reflect import Type
+import com.xebialabs.deployit.engine.api.dto.ValidatedConfigurationItem
 
 from Base import Base
 
 import datetime
 
 # Globals
-
 __repositoryService = XLReleaseServiceHolder.getRepositoryService()
+__ciType = 'rel.ReleaseCounterStore'
+
 StorageTimestamp = " "
 
+
+
+def get_ci_object(ciType, id):
+    if not DescriptorRegistry.exists(Type.valueOf(ciType)):
+        raise ValueError('Unknown CI type %s' % ciType)
+    type = Type.valueOf(ciType)
+    configurationItem = type.descriptor.newInstance(id)
+    return configurationItem
+
+def checkForValidations(ci):
+    if isinstance(ci, com.xebialabs.deployit.engine.api.dto.ValidatedConfigurationItem) and not ci.validations.isEmpty():
+        raise ValueError('Configuration item contained validation errors: %s' % ci.validations.toString())
+
+def create_ci(storeName):
+    '''
+    creates a ci in the xl-release repository under /Configuration/Custom of type rel.ReleaseCounterStore
+    :param storeName:
+    :return:
+    '''
+    Base.info("creating Counter Store: %s " % storeName)
+
+    ci = get_ci_object(__ciType,  'Configuration/Custom/%s' % storeName)
+
+    ci.setTitle(storeName)
+
+    checkForValidations(ci)
+
+    try:
+        __repositoryService.create(ci)
+        Base.info("Counter Store: %s created")
+    except com.xebialabs.deployit.repository.ItemAlreadyExistsException as e:
+        Base.error(e)
+        Base.error("Counter Store already exists... moving on")
+    except Exception:
+        Base.fatal('an error occured while trying to create Counter Store: %s' % storeName)
+
+def destroy_counter_store(storeName):
+    '''
+    Destroys a release counter store
+    :param storeName:
+    :return:
+    '''
+
+    if ciExists(storeName, __ciType):
+
+        ci = loadCiFromRepo(storeName, __ciType )
+
+        __repositoryService.delete(ci.getId())
+        Base.info("Counter Store: %s destroyed.... BOOOM" % storeName)
+
+    else:
+        Base.error("Counter Store: %s did not exist... Moving on cuz that is how we roll" % storeName)
 
 
 def time_stamp():
@@ -28,7 +84,7 @@ def get_counter_storage(counterStoreTitle, updateTimeStamp=True):
     global StorageTimestamp
 
 
-    counter_store_ci = loadCiFromRepo(counterStoreTitle, 'rel.ReleaseCounterStore')
+    counter_store_ci = loadCiFromRepo(counterStoreTitle, __ciType)
     if updateTimeStamp == True:
         StorageTimestamp = counter_store_ci.getProperty('modTime')
 
@@ -36,7 +92,7 @@ def get_counter_storage(counterStoreTitle, updateTimeStamp=True):
 
 def get_counter_timestamp(counterStoreTitle):
 
-    return loadCiFromRepo(counterStoreTitle, 'rel.ReleaseCounterStore').getProperty('modTime')
+    return loadCiFromRepo(counterStoreTitle, __ciType).getProperty('modTime')
 
 def update_counter_storage(counterStoreTitle, key, value):
 
@@ -68,7 +124,7 @@ def updateCiToRepo(storeName, data):
 
     global StorageTimestamp
     # get the store
-    store = loadCiFromRepo(storeName, 'rel.ReleaseCounterStore')
+    store = loadCiFromRepo(storeName, __ciType)
 
     # set the properties on the ci to be updated
     for k, v in data.items():
@@ -101,11 +157,23 @@ def loadCiFromRepo(storeName, type):
     sp.setType(Type.valueOf(type))
 
     for p in __repositoryService.listEntities(sp):
-
+        print p.getId()
         if str(p.getTitle()) == storeName:
             return p
 
     Base.fatal("unable to find json data repository: %s" % storeName)
+
+def ciExists(storeName, type):
+
+    sp = SearchParameters()
+    sp.setType(Type.valueOf(type))
+
+    for p in __repositoryService.listEntities(sp):
+
+        if str(p.getTitle()) == storeName:
+            return True
+
+    return False
 
 
 def create_or_update_counter(counterStoreTitle, key, value):
@@ -133,6 +201,12 @@ elif taskAction == 'increment':
     outputVariable = get_counter_value(counterStore, counterName)
     outputVariable = outputVariable + 1
     update_counter_storage(counterStore, counterName, outputVariable)
+elif taskAction == 'createStore':
+    Base.info("creating release counterstore: %s" % counterStore )
+    create_ci(counterStore)
+elif taskAction == 'destroyStore':
+    Base.info("Destroying release counterstore: %s" % counterStore )
+    destroy_counter_store(counterStore)
 else:
     Base.info("nothing to do ")
 
