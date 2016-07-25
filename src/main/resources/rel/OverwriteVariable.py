9fe52d9852138d95da0a25310ed4c9b7a1c130ab
wianvos <wvos@xebia.com>
2016-07-25 10:08:02 -0400

    added variable overwrite capacity

--- src/main/resources/rel/OverwriteVariable.py
+++ src/main/resources/rel/OverwriteVariable.py
@@ -10,21 +10,23 @@ __variable_start_regex = re.compile('^\$\{', re.IGNORECASE)
 # grab the current release
 __release = getCurrentRelease()
 
-# grab the variable dictionary
-variables = __release.getVariableValues()
+if overwriteValue != "None":
+        
+    # grab the variable dictionary
+    variables = __release.getVariableValues()
 
-#check that the variable is properly formatted
-if re.match(__variable_start_regex, variableName) is None:
-        key = "${%s}" % (variableName)
+    #check that the variable is properly formatted
+    if re.match(__variable_start_regex, variableName) is None:
+            key = "${%s}" % (variableName)
 
-# set the overwrite
-variables[key] = overwriteValue
-
-
-# update the new variable dict in the release
-__release.setVariableValues(variables)
+    # set the overwrite
+    variables[key] = overwriteValue
 
 
-# update the actual running release
-releaseApi.updateRelease(release.id, __release)
+    # update the new variable dict in the release
+    __release.setVariableValues(variables)
+
+
+    # update the actual running release
+    releaseApi.updateRelease(release.id, __release)
 
